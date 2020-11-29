5c6dd199f36846c2b55260d313e370b1a201d070
lhfaguiar <lais.harumi.f@gmail.com>
2020-11-29 18:35:45 -0300

    preciso estudar sintaxes

---
+++ SW/R/gerador_node/hidden_node.py
@@ -0,0 +1,17 @@
+"""
+Esse arquivo gera o no de camada escondida
+"""
+
+# Imports
+import pandas as pd
+import math
+
+
+def Hidden_node_gen():
+    """
+    Gera um no de camada escondida da rede neural
+    """
+    node = ''
+
+
+    return node
---
+++ activation_fuction/tgh.py
@@ -0,0 +1,4 @@
+from math import atan
+
+def tgh(x):
+  return atanh(x)
